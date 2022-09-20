5c8547106f2b8cf2eec77a6baefdbb4c9801b0da
Ahnset <ahnset24@gmail.com>
2022-09-20 09:30:49 +0000

    clean - switch to gradle version catalog.

--- buildSrc/src/main/java/ConfigData.kt
+++
@@ -1,7 +0,0 @@
-object ConfigData {
-    const val compileSdk = 32
-    const val targetSdk = 32
-    const val minSdk = 21
-    const val versionCode = 1
-    const val versionName = "1.0"
-}
--- buildSrc/src/main/java/TestDependencies.kt
+++
@@ -1,8 +0,0 @@
-object TestDependencies {
-    const val junit4 = "junit:junit:${Versions.junit}"
-    const val mockk = "io.mockk:mockk:${Versions.mockk}"
-    const val junitExt = "androidx.test.ext:junit:${Versions.junitExt}"
-    const val espresso = "androidx.test.espresso:espresso-core:${Versions.espresso}"
-    const val hiltAndroidTesting = "com.google.dagger:hilt-android-testing:${Versions.hiltCore}"
-    const val hiltCompiler = "com.google.dagger:hilt-compiler:${Versions.hiltCore}"
-}
