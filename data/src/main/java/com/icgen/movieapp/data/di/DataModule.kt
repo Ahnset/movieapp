b345e8155ed0b765f75db4017a16da71a31edb1e
Ahnset <ahnset24@gmail.com>
2022-09-20 14:45:15 +0000

    Add home feature dependency injection in data module.

---
+++ data/src/main/java/com/icgen/movieapp/data/di/DataModule.kt
@@ -0,0 +1,16 @@
+package com.icgen.movieapp.data.di
+
+import com.example.movieapp.core.repository.HomeRepository
+import com.icgen.movieapp.data.HomeRepositoryImpl
+import dagger.Binds
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+
+@Module
+@InstallIn(SingletonComponent::class)
+abstract class DataModule {
+
+    @Binds
+    abstract fun bindHomeRepository(impl: HomeRepositoryImpl): HomeRepository
+}
