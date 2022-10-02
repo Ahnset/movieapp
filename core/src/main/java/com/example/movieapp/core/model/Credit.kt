974cb90278983615b61a10b97d125eca6ec70140
Ahnset <ahnset24@gmail.com>
2022-10-02 11:16:09 +0000

    Add presentation models for detail feature.

--- core/src/main/java/com/example/movieapp/core/model/Credit.kt
+++
@@ -1,6 +0,0 @@
-package com.example.movieapp.core.model
-
-data class Credit(
-    val id: Int,
-    val cast: List<Cast>
-)
--- core/src/main/java/com/example/movieapp/core/model/Preview.kt
+++
@@ -1,6 +0,0 @@
-package com.example.movieapp.core.model
-
-data class Preview(
-    val id: Int,
-    val results: List<Video>,
-)
