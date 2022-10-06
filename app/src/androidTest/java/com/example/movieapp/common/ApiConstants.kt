926bd39711dfdaf16a6e2d2d13a860ae70458c8e
Ahnset <ahnset24@gmail.com>
2022-10-06 18:41:07 +0000

    Test detail presentation ui.

--- app/src/androidTest/java/com/example/movieapp/common/ApiConstants.kt
+++ app/src/androidTest/java/com/example/movieapp/common/ApiConstants.kt
@@ -3,4 +3,7 @@ package com.example.movieapp.common
 object ApiConstants {
     const val BASE_URL = "http://localhost:8080/"
     const val TRENDING_RESPONSE_FILE = "trending_response.json"
+    const val DETAIL_RESPONSE_FILE = "detail_response.json"
+    const val CREDITS_RESPONSE_FILE = "credits_response.json"
+    const val VIDEOS_RESPONSE_FILE = "videos_response.json"
 }
